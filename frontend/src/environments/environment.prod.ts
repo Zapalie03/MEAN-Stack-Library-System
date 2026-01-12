4856677be34af2be844cd6713ef9123cb3442896
Abdoulie Bah <bahablie03@gmail.com>
2026-01-12 21:09:55 +0000

    Update production API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Will be set during deployment - use Render backend URL
-  apiUrl: 'https://library-backend.onrender.com/api'
+  apiUrl: 'https://mean-stack-library-system.onrender.com'
 };
